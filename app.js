c1de6e68816a5f02fb6d7c41cbc57761d6fa8c6b
thainee <thaivh9@fpt.com>
2024-08-02 00:32:05 +0700

    feat(get tours): starting our first api handing get tours request

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
@@ -11,6 +12,20 @@ const app = express();
 // app.post('/', (req, res) => {
 //   res.send('You can post to this endpoint');
 // });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
 
 const port = 3000;
 app.listen(port, () => {
