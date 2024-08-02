f7e249bc2372b95fd10c93ca2556da4b4ec9e9ad
thainee <thaivh9@fpt.com>
2024-08-02 10:27:25 +0700

    refactor: file path syntax, feat: handling post request

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 const fs = require('fs');
 const express = require('express');
+const path = require('path');
 
 const app = express();
+const port = 3000;
 
-// app.get('/', (req, res) => {
-//   res
-//     .status(200)
-//     .json({ message: 'Hello from the server side!', app: 'Natours' });
-// });
+app.use(express.json());
 
-// app.post('/', (req, res) => {
-//   res.send('You can post to this endpoint');
-// });
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+const toursFilePath = path.join(
+  __dirname,
+  'dev-data',
+  'data',
+  'tours-simple.json'
 );
+
+const tours = JSON.parse(fs.readFileSync(toursFilePath));
 
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
@@ -27,7 +26,24 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
-const port = 3000;
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = { ...req.body, id: newId };
+
+  tours.push(newTour);
+
+  fs.writeFile(toursFilePath, JSON.stringify(tours), (err) => {
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  });
+});
+
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
