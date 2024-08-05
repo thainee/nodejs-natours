5da0e6c38d218672b780435853b41312e2e298f4
thainee <thaivh9@fpt.com>
2024-08-06 00:19:37 +0700

    build: setting up mongoose, mongodb connection

--- server.js
+++ server.js
@@ -1,7 +1,31 @@
+import mongoose from 'mongoose';
 import app from './app.js';
 
-const port = process.env.PORT;
+const uri = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
+const clientOptions = {
+  serverApi: { version: '1', strict: true, deprecationErrors: true },
+};
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } catch (error) {
+    console.error('DB connection failed:', error.message);
+    process.exit(1);
+  }
+};
+
+connectDB();
+
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
