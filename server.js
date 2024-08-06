283dc7de2ca474487442986d3e8d013e441304ed
thainee <thaivh9@fpt.com>
2024-08-06 23:44:10 +0700

    learn: Creating a Simple Tour Model; Creating Documents and Testing the Model

--- server.js
+++ server.js
@@ -21,9 +21,39 @@ const connectDB = async () => {
     console.error('DB connection failed:', error.message);
     process.exit(1);
   }
+  // finally {
+  //   // Ensures that the client will close when you finish/error
+  //   await mongoose.disconnect();
+  // }
 };
 
 connectDB();
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: { type: Number, default: 4.5 },
+  price: { type: Number, required: [true, 'A tour must have a price'] },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('ERROR :', err);
+  });
 
 const port = process.env.PORT;
 app.listen(port, () => {
