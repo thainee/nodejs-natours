294e1e788a94712514069a3895634a355bc40e78
thainee <thaivh9@fpt.com>
2024-08-08 20:04:26 +0700

    refactor (tour): modelling the tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -50,7 +50,7 @@ export const createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: 'Invalid data sent!',
+      message: err,
     });
   }
 };
