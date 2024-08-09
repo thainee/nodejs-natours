205a256c042c76d5e9570e087135dde0fe1b07d2
thainee <thaivh9@fpt.com>
2024-08-09 22:29:26 +0700

    feat(tour controller): making the api better filtering with query

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,8 +2,23 @@ import Tour from '../models/tourModel.js';
 
 export const getAllTour = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
 
+    const query = Tour.find(queryObj);
+
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
