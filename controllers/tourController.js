29ae1d35c474a42777fb3ab39b569ae5ab12b716
thainee <thaivh9@fpt.com>
2024-08-10 18:20:53 +0700

    feat(fields): making the api better limiting fields

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -22,6 +22,14 @@ export const getAllTour = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    // 2) Sorting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v')
+    }
+
     // EXECUTE QUERY
     const tours = await query;
 
