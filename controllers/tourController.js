812ca5d76fbd555cb9e9da9dd1384634d48116b1
thainee <thaivh9@fpt.com>
2024-08-10 21:07:27 +0700

    feat(pagination): making the api better pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -22,12 +22,25 @@ export const getAllTour = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
-    // 2) Sorting
+    // 3) Field limiting
     if (req.query.fields) {
       const fields = req.query.fields.split(',').join(' ');
       query = query.select(fields);
     } else {
-      query = query.select('-__v')
+      query = query.select('-__v');
+    }
+
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 10;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const toursCount = await Tour.countDocuments();
+
+      if (skip >= toursCount) throw new Error('Page does not exist');
     }
 
     // EXECUTE QUERY
