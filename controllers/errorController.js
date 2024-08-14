79fdb42713a1e198aab6aa41a4d6a99672e533b2
thainee <thaivh9@fpt.com>
2024-08-15 00:26:59 +0700

    refactor(error controller): errors during development vs production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,10 +1,40 @@
-export const globalErrorHandler = (err, req, res, next) => {
-  console.log(err.stack);
-  const statusCode = err.statusCode || 500;
-  const status = err.status || 'error';
-
-  res.status(statusCode).json({
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
     status,
+    error: err,
     message: err.message,
+    stack: err.stack,
   });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status,
+      message: err.message,
+    });
+
+    // Programming or other unknown error: dont't leak error details
+  } else {
+    // 1) Log error
+    console.error('Error 💥', err);
+
+    // 1) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong!',
+    });
+  }
+};
+
+export const globalErrorHandler = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
