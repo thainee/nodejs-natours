f5b5592ac906c10fd3f144305fa20bea6dc9b8af
thainee <thaivh9@fpt.com>
2024-08-17 12:42:25 +0700

    feat(error): errors outside express unhandled rejections

--- server.js
+++ server.js
@@ -11,16 +11,14 @@ const clientOptions = {
 };
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(uri, clientOptions);
-    await mongoose.connection.db.admin().command({ ping: 1 });
-    console.log(
-      'Pinged your deployment. You successfully connected to MongoDB!'
-    );
-  } catch (error) {
-    console.error('DB connection failed:', error.message);
-    process.exit(1);
-  }
+  // try {
+  await mongoose.connect(uri, clientOptions);
+  await mongoose.connection.db.admin().command({ ping: 1 });
+  console.log('Pinged your deployment. You successfully connected to MongoDB!');
+  // } catch (error) {
+  //   console.error('DB connection failed:', error);
+  //   process.exit(1);
+  // }
   // finally {
   //   // Ensures that the client will close when you finish/error
   //   await mongoose.disconnect();
@@ -30,6 +28,14 @@ const connectDB = async () => {
 connectDB();
 
 const port = process.env.PORT;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('Unhandled Rejection! 💥 Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
