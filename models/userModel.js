0f32c2c9a5f95e1e2b77b204accd2f325256a66e
thainee <thaivh9@fpt.com>
2024-08-24 23:32:00 +0700

    feat(user): modelling users

---
+++ models/userModel.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+import validator from 'validator';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email address'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email address'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: [8, 'Password must be at least 8 characters long'],
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
