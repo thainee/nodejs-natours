6fa6d92fd1c0a244a1ff2ee2b6017c99498de027
thainee <thaivh9@fpt.com>
2024-09-01 23:18:02 +0700

    feat(user): signing up user with jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,22 @@
+import jwt from 'jsonwebtoken';
 import User from './../models/userModel.js';
 import catchAsync from '../utils/catchAsync.js';
 
 export const signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
