22acfd77e06038d19f06aba4a722fff92f544403
thainee <thaivh9@fpt.com>
2024-09-13 00:51:33 +0700

    feat(sanitization): data sanitization by xss and express-mongo-sanitize

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import morgan from 'morgan';
 import { rateLimit } from 'express-rate-limit';
 import helmet from 'helmet';
+import xss from 'xss';
+import mongoSanitize from 'express-mongo-sanitize';
 
 import AppError from './utils/appError.js';
 import { globalErrorHandler } from './controllers/errorController.js';
@@ -28,7 +30,20 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
-app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use((req, res, next) => {
+  Object.keys(req.body).forEach((key) => {
+    if (typeof req.body[key] === 'string') {
+      req.body[key] = xss(req.body[key]);
+    }
+  });
+  next();
+});
 
 // Test middleware
 app.use((req, res, next) => {
