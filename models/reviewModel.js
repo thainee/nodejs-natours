7c48a057743e246904426df10e9ae1c6cec0002a
thainee <thaivohong2003@gmail.com>
2024-09-19 00:51:57 +0700

    feat (tour): calculating average ratings on tours - when update and delete review

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -59,14 +59,31 @@ reviewSchema.statics.calcAverageRatings = async function (tourId) {
     },
   ]);
 
-  await Tour.findByIdAndUpdate(tourId, {
-    ratingsQuantity: stats[0].nRating,
-    ratingsAverage: stats[0].avgRating,
-  });
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5,
+    });
+  }
 };
 
-reviewSchema.post('save', function () {
-  this.constructor.calcAverageRatings(this.tour);
+reviewSchema.post('save', function (doc) {
+  doc.constructor.calcAverageRatings(doc.tour);
+});
+
+// findByIdAndUpdate
+// findByIdAndDelete
+reviewSchema.post(/^findOneAnd/, function (doc) {
+  doc.constructor.calcAverageRatings(doc.tour);
+});
+
+reviewSchema.post(/^findOneAnd/, function (doc) {
+  doc.constructor.calcAverageRatings(doc.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
