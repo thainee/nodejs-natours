f52f60e8a289024c3a90cd0bad315ce2ef2a62bb
thainee <thaivohong2003@gmail.com>
2024-09-22 14:12:57 +0700

    feat (tour overview pug): building the tour overview

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,8 +1,18 @@
-export const getOverview = (req, res) => {
+import Tour from '../models/tourModel.js';
+import catchAsync from '../utils/catchAsync.js';
+
+export const getOverview = catchAsync(async (req, res, next) => {
+  // 1) Get tour data from collection
+  const tours = await Tour.find();
+
+  // 2) Build template
+
+  // 3) Render that template using tour data from 1)
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 
 export const getTour = (req, res) => {
   res.status(200).render('tour', {
