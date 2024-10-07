f77594c98be09705428d1f4b0870f275b6611cf9
thainee <thaivohong2003@gmail.com>
2024-10-07 23:47:38 +0700

    feat (booking): modelling the bookings

---
+++ models/bookingModel.js
@@ -0,0 +1,35 @@
+import mongoose, { Schema } from 'mongoose';
+
+const bookingSchema = new Schema(
+  {
+    tour: {
+      type: Schema.Types.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Booking must belong to a tour.'],
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'Booking must belong to a user.'],
+    },
+    price: {
+      type: Number,
+      required: [true, 'Booking must have a price.'],
+    },
+    paid: {
+      type: Boolean,
+      dafault: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+bookingSchema.pre(/^find/, function (next) {
+  this.populate([{ path: 'tour', select: 'name' }, 'user']);
+});
+
+const Booking = mongoose.model('Booking', bookingSchema);
+
+export default Booking;
